c866ac4a8d1c5b86aec65dcbe4b3c1d9e9d893da
Nkunja~ <munene01denis@gmail.com>
2023-12-02 10:31:28 +0300

    Resume

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import Link from 'react';
+
 
 const resume = () => {
+  const documentURL = 'https://docs.google.com/document/d/1GlEKPtBHlsXoryvjDgEyKfTa9C30FjnH/edit?usp=drive_link&ouid=102473385364013312680&rtpof=true&sd=true';
   return (
     <>
       <Head>
@@ -34,88 +37,10 @@ const resume = () => {
             </a>
           </div>
         </div>
-        <div className='text-center py-4 text-xl font-bold uppercase tracking-wider'>
-          <div className='hidden sm:block'>
-            <p>
-              Proven Leadership <span className='px-1'>|</span> Web Development{' '}
-              <span className='px-1'>|</span> Complex Problem Solving
-            </p>
-          </div>
-          <div className='block sm:hidden'>
-            <p>Proven Leadership</p>
-            <p className='py-2'>Web Development</p>
-            <p>Complex Problem Solving</p>
-          </div>
+        <div>
+          <iframe src={documentURL} width="100%" height="600px" />
         </div>
-        <p>
-          Analytical, innovative, motivated and self-driven web development professional
-          with the desire to grow and learn more on team leadership and
-          organizational effectiveness in fast-paced and challenging
-          environments. Adept at developing strategies and driving streamlined
-          operations. Diverse analytical skills, team collaboration, and
-          relationship building. Consummate professional, and motivated leader,
-          with solid interpersonal abilities and complex problem-solving skills.
-          Effective and proven track record of critical thinking, idea
-          generation, and optimizing efficiencies.
-        </p>
-
-        {/* Skills */}
-        <div className='text-center py-4'>
-          <h5 className='text-center underline text-[18px] py-2'>Skills</h5>
-          <p className='py-2'>
-            <span className='font-bold'>Technical Skills</span>
-            <span className='px-2'>|</span>Front-End Web Developer
-            <span className='px-2'>|</span> HTML
-            <span className='px-2'>|</span>CSS
-            <span className='px-2'>|</span>Javascript
-            <span className='px-2'>|</span>React
-            <span className='px-2'>|</span>Next JS
-            <span className='px-2'>|</span>SQL
-            <span className='px-2'>|</span>NoSQL
-            <span className='px-2'>|</span>Redux
-            <span className='px-2'>|</span>Tailwind
-            <span className='px-2'>|</span> Firebase
-            <span className='px-2'>|</span> RESTAPI
-          </p>
-          <p className='py-2'>
-            <span className='font-bold'>Amazon Web Services</span>
-            <span className='px-2'>|</span>Amazon Web Services Cloud
-            Practitioner
-          </p>
-        </div>
-
-        <h5 className='text-center underline text-[18px] py-4'>
-          Professional Experience
-        </h5>
-        {/* Experience */}
-        <div className='py-6'>
-          <p className='italic'>
-            <span className='font-bold italic'>
-            Freelance Web Development
-            </span>
-            <span className='px-2'>|</span>Nairobi, Kenya
-          </p>
-          <p className='py-1 italic'>Web Developer(2020 - Current)</p>
-          <ul className='list-disc list-outside px-7 py-1 leading-relaxed'>
-          <li>
-              Developed well responsive web applications majoring on front end technologies such as React JS and Next JS and backend
-              technologies such as Node JS and PHP
-            </li>
-            <li>
-              Ensured comprehensive mobile responsive UI layouts with
-              understanding of HTML, CSS, and JavaScript.
-            </li>
-            <li>
-              Troubleshoot issues and concerns, configure operation and analytic
-              plugins, and design changes as needed.
-            </li>
-            <li>
-              Worked directly with a diverse client base to understand and
-              implement multiple technologies and programs.
-            </li>
-            
-          </ul>
-        </div>
+        
       </div>
     </>
   );
